f8f17731c4449b5d2e0235d5cd5adbe577886ea1
unknown <ajmore.biz@gmail.com>
2014-12-20 17:03:24 +0530

    print

--- web/index.php
+++ web/index.php
@@ -26,7 +26,9 @@ $app->get('/', function() use($app) {
     $adapter = $hybridauth->authenticate("Facebook");
     $user_profile = $adapter->getUserProfile();
     echo "<pre>";
-    print_r($userProfile);
+    var_dump($userProfile);
+    var_dump($hybridauth_session_data);
+    echo 'neat';
     echo "</pre>";
     return 'Hello';
 });
